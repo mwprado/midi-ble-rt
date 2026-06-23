04daeb41589d7eeb4231aad0c4f96cbd1853ff38
Moacyr Prado <mwprado@users.noreply.github.com>
2026-06-23 14:28:40 -0300

    Add frame model placeholder

---
+++ src/mb-frame-model.h
@@ -0,0 +1,15 @@
+#ifndef MB_FRAME_MODEL_H
+#define MB_FRAME_MODEL_H
+
+#include <stdint.h>
+
+#define MB_FRAME_MODEL_COUNT 32u
+#define MB_FRAME_MODEL_CAPACITY 256u
+
+typedef struct {
+    uint16_t id;
+    uint16_t off;
+    uint16_t len;
+} MbFrameModelSlice;
+
+#endif
