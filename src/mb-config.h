b841edc1651630ad93045c4784c5d06886d61e8b
Moacyr Prado <mwprado@users.noreply.github.com>
2026-06-27 12:25:10 -0300

    Add multi-device config model

--- src/mb-config.h
+++ src/mb-config.h
@@ -1,7 +1,22 @@
 #ifndef MB_CONFIG_H
 #define MB_CONFIG_H
 
+#include <glib.h>
 #include <stdbool.h>
+
+typedef struct MbDeviceConfig {
+    char *id;
+    char *address;
+    char *name;
+    char *profile;
+    char *alsa_port_name;
+    bool enabled;
+    bool autoconnect;
+    bool pair;
+    bool trust;
+    bool auto_reconnect;
+    bool enable_tx;
+} MbDeviceConfig;
 
 typedef struct MbConfig {
     char *address;
@@ -21,9 +36,22 @@ typedef struct MbConfig {
     bool enable_tx;
     bool stats_enabled;
     unsigned stats_interval_ms;
+
+    /*
+     * Multi-device configuration model.
+     *
+     * Existing single-device configs using [device] remain valid. When no
+     * devices.d/*.ini entries are loaded, a single compatibility device is
+     * synthesized from [device] if address is set.
+     */
+    GPtrArray *devices; /* element type: MbDeviceConfig* */
 } MbConfig;
 
 bool mb_config_load(MbConfig *cfg, const char *path);
+bool mb_config_load_dir(MbConfig *cfg, const char *dir_path);
 void mb_config_clear(MbConfig *cfg);
+
+unsigned mb_config_device_count(const MbConfig *cfg);
+const MbDeviceConfig *mb_config_get_device(const MbConfig *cfg, unsigned index);
 
 #endif
