4e147139e77e725e94c2739cff7b696eda549ec9
Moacyr Prado <mwprado@users.noreply.github.com>
2026-06-28 11:13:09 -0300

    Add BlueZ device lookup by address API

--- src/mb-bluez.h
+++ src/mb-bluez.h
@@ -21,6 +21,7 @@
 #define GATT_CHRC_IFACE        "org.bluez.GattCharacteristic1"
 
 GVariant *mb_bluez_get_managed_objects(GDBusConnection *bus);
+char *mb_bluez_find_device_path_by_address(GDBusConnection *bus, const char *address);
 bool mb_bluez_get_device_bool_property(GDBusConnection *bus, const char *device_path, const char *property, bool *out);
 bool mb_bluez_set_device_trusted(GDBusConnection *bus, const char *device_path);
 bool mb_bluez_pair_device(GDBusConnection *bus, const char *device_path);
