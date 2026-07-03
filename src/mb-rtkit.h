5e02feeba8e803c7c6404c8d8542af4776db5105
Moacyr Prado <mwprado@users.noreply.github.com>
2026-07-03 15:37:49 -0300

    Add directional RtKit worker options

--- src/mb-rtkit.h
+++ src/mb-rtkit.h
@@ -10,8 +10,13 @@ G_BEGIN_DECLS
 #define MB_RTKIT_DEFAULT_PRIORITY 1u
 #define MB_RTKIT_MAX_PRIORITY 20u
 
-void mb_rtkit_configure(bool enabled, unsigned priority);
+void mb_rtkit_configure(bool enabled,
+                        unsigned priority,
+                        bool realtime_rx,
+                        bool realtime_tx);
 bool mb_rtkit_enabled(void);
+bool mb_rtkit_realtime_rx_enabled(void);
+bool mb_rtkit_realtime_tx_enabled(void);
 unsigned mb_rtkit_priority(void);
 void mb_rtkit_make_current_thread_realtime(const char *thread_name);
 
