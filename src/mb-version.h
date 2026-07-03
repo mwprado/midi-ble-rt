ac147cd30954fcbb962e7f70c731ee2bd0358c57
Moacyr Prado <mwprado@users.noreply.github.com>
2026-07-03 17:29:37 -0300

    Remove static version header

--- src/mb-version.h
+++
@@ -1,9 +0,0 @@
-#ifndef MB_VERSION_H
-#define MB_VERSION_H
-
-#define MB_VERSION "0.9.2"
-#define MB_VERSION_MAJOR 0
-#define MB_VERSION_MINOR 9
-#define MB_VERSION_PATCH 2
-
-#endif /* MB_VERSION_H */
