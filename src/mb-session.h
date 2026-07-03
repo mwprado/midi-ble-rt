f7c805a616537ace426c14e5c53e0acb190354bd
Moacyr Prado <mwprado@users.noreply.github.com>
2026-07-03 20:23:34 -0300

    Remove legacy session buffers from session model

--- src/mb-session.h
+++ src/mb-session.h
@@ -6,8 +6,6 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#include "mb-buffer.h"
-
 G_BEGIN_DECLS
 
 typedef enum {
@@ -107,8 +105,6 @@ typedef struct {
     unsigned reconnect_attempts;
 
     uint8_t running_status;
-
-    MbSessionBuffers buffers;
 } MbSession;
 
 typedef struct {
